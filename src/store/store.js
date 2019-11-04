0eda5c1187a7a2ae5ca5aeb9cf59b592fe4a40e9
lukem512 <hi@lukemitchell.co>
2019-11-04 22:31:57 +0000

    Throttle state update

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux'
+import throttle from 'lodash.throttle'
+
 import reducers from './reducers'
 import { loadState, saveState } from './localStorage'
 
@@ -6,7 +8,8 @@ const persistedState = loadState()
 const store = createStore(reducers, persistedState)
 
 // Subscribe to state events
-// and save them to localStorage
-store.subscribe(() => saveState(store.getState()))
+// and save them to localStorage.
+// Throttle to only update once per second, maximum
+store.subscribe(throttle(() => saveState(store.getState()), 1000))
 
 export default store
