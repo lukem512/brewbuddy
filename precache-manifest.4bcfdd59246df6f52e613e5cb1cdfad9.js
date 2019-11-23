a478408ac3330c01ac0ffb77220da0f9470b4326
lukem512 <hi@lukemitchell.co>
2019-11-23 10:30:19 +0000

    Updates

---
+++ precache-manifest.4bcfdd59246df6f52e613e5cb1cdfad9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c17468fea468045f5ec7d343434f5f",
+    "url": "/brewbuddy/index.html"
+  },
+  {
+    "revision": "e6abb04768db42ca254e",
+    "url": "/brewbuddy/static/js/2.e88bbc5e.chunk.js"
+  },
+  {
+    "revision": "bfbb6cdc7aeb23195c40",
+    "url": "/brewbuddy/static/js/main.fda27a0f.chunk.js"
+  },
+  {
+    "revision": "b6c030777415b47913d1",
+    "url": "/brewbuddy/static/js/runtime-main.4c0a0f2d.js"
+  }
+]);
